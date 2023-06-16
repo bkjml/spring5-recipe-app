058320a6d86656ca7bc219be545cc0e197ef095f
Matt <mbirhane@kifiya.et>
2023-06-16 11:15:42 +0300

    create recipe and notes

---
+++ src/main/java/guru/springframwork/recipe/controllers/indexController.java
@@ -0,0 +1,13 @@
+package guru.springframwork.recipe.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class indexController {
+
+    @RequestMapping({"", "/", "/index", "/index.html"})
+    public String getIndexPage(){
+        return "index";
+    }
+}
